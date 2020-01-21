ead3478cd80d1caa66078a733acdbf2a79cdf992
Wilhelm <ga46yeg@mytum.de>
2020-01-21 14:23:58 +0100

    some tests

--- deeptool/_nbdev.py
+++ deeptool/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"test": "00_core.ipynb"}
+index = {"test": "00_core.ipynb",
+         "test2": "00_core.ipynb"}
 
 modules = ["test.py"]
 
