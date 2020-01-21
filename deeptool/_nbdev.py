eae41ff657209cc4944703190347d1e7c7ea12b7
Wilhelm <ga46yeg@mytum.de>
2020-01-21 16:29:27 +0100

    renamed to demo

--- deeptool/_nbdev.py
+++ deeptool/_nbdev.py
@@ -2,10 +2,10 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"test": "00_core.ipynb",
-         "test2": "00_core.ipynb"}
+index = {"test": "00_demo.ipynb",
+         "test2": "00_demo.ipynb"}
 
-modules = ["core.py"]
+modules = ["demo.py"]
 
 doc_url = "https://NikonPic.github.io/deeptool/"
 
